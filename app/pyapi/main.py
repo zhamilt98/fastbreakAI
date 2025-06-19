df772c570513794ff970d66a7f2a27b313e5aaf6
zhamilt98 <zhamilt98@gmail.com>
2025-06-19 13:17:52 -0400

    allow all origins for debugging

--- app/pyapi/main.py
+++ app/pyapi/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000','https://fastbreak-ai.vercel.app/'],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
